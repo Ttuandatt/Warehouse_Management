2806769fff04cf3f29945f4a69e51269d29ebf4b
Ttuandatt <139537793+Ttuandatt@users.noreply.github.com>
2024-03-11 17:14:20 +0700

    Add files via upload

---
+++ LoginModel.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ACER
+ */
+public class LoginModel {
+    private String username;
+    private String password;
+    //Constructor
+    public LoginModel(){
+    }
+    public LoginModel(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    //getter, setter
+    public String getUsername(){
+        return this.username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
