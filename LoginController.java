14e3626d8e7e6a88b407965f450aacfb89c78607
Ttuandatt <139537793+Ttuandatt@users.noreply.github.com>
2024-03-11 17:13:37 +0700

    Add files via upload

---
+++ LoginController.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import View.LoginView;
+import View.NhanVienKhoView;
+import Model.LoginModel;
+import View.QuanLyKhoView;
+import javax.swing.JOptionPane;
+public class LoginController {
+    public LoginView view;
+    //Constructor
+    public LoginController(LoginView view){
+        this.view = view;
+    }
+    public void checkAndLogin(LoginModel user){
+    if((user.getUsername().equals("admin")) && user.getPassword().equals("123")){
+        QuanLyKhoView qlk = new QuanLyKhoView();
+        qlk.khoiTaoGiaoDien(); // Hiển thị frame quanLyKho
+        // Đóng frame đăng nhập
+        view.closeLoginFrame();
+    }
+    else if(user.getUsername().equals("nv1") && user.getPassword().equals("123")){
+        NhanVienKhoView nvk = new NhanVienKhoView();
+        nvk.khoiTaoGiaoDien(); // Đảm bảo khởi tạo giao diện cho frame nhanVienKho
+        // Đóng frame đăng nhập
+        view.closeLoginFrame();
+    }
+    else{
+       JOptionPane.showMessageDialog(null, "Sai thông tin đăng nhập!", "Thông báo", JOptionPane.ERROR_MESSAGE);
+    }
+}
+    
+}
+
