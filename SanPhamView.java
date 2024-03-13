820068eadd5104b764f90f9c9133e84771150b6f
Ttuandatt <139537793+Ttuandatt@users.noreply.github.com>
2024-03-13 20:51:17 +0700

    Phân chia bố cục các nút chức năng ở panel leftFunction

--- SanPhamView.java
+++ SanPhamView.java
@@ -3,10 +3,12 @@ package View;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
@@ -27,9 +29,8 @@ public class SanPhamView extends JPanel {
     private String xuatXu;
     
     //Constructor
-    public SanPhamView(){
-    }
     public void giaoDienSanPham(){
+        setLayout(new FlowLayout());
         setBackground(Color.decode("#6c757d")); 
         setLayout(new GridBagLayout());
         
@@ -57,7 +58,6 @@ public class SanPhamView extends JPanel {
         JPanel leftFunction = new JPanel();
         leftFunction.setBackground(Color.RED);
         leftFunction.setBorder(BorderFactory.createTitledBorder("Chức năng"));
-//        leftFunction.setBorder(BorderFactory.createTi);
         JPanel rightFunction = new JPanel();
         rightFunction.setBackground(Color.BLUE);
         rightFunction.setBorder(BorderFactory.createTitledBorder("Tìm kiếm"));
@@ -73,7 +73,7 @@ public class SanPhamView extends JPanel {
         gbc.fill = GridBagConstraints.BOTH;
         functionBar.add(rightFunction, gbc);
         
-        //Tạo bảng
+        //Tạo bảng, phần này sau khi có database sẽ lấy data từ đó
         String[][] data = {
                 { "AC1", "Laptop Acer Nitro 5", "20", "25.000.000đ"},
                 { "AS1", "Laotop Asus TUF", "15", "23.000.000"}
@@ -97,9 +97,10 @@ public class SanPhamView extends JPanel {
         gbc.fill = GridBagConstraints.BOTH;
         content.add(sp, gbc);
         
-        leftFunction.setLayout(new GridBagLayout());
+        leftFunction.setLayout(null);
         rightFunction.setLayout(new GridBagLayout());
         
+        //Tạo và thêm các panel chứa các nút chức năng ở leftFunction
         JPanel panelThem = new JPanel();
         JPanel panelXoa = new JPanel();
         JPanel panelSua = new JPanel();
@@ -107,22 +108,84 @@ public class SanPhamView extends JPanel {
         panelThem.setBackground(Color.GRAY);
         panelXoa.setBackground(Color.CYAN);
         panelSua.setBackground(Color.ORANGE);
-        panelXuatExcel.setBackground(Color.DARK_GRAY);
- 
+        panelXuatExcel.setBackground(Color.PINK);
         
+        panelThem.setBounds(20, 20, 60, 60);
+        panelThem.setLayout(null);
+        panelXoa.setBounds(90,20,60,60);
+        panelXoa.setLayout(null);
+        panelSua.setBounds(160,20,60,60);
+        panelSua.setLayout(null);
+        panelXuatExcel.setBounds(260,20,70,60);
+        panelXuatExcel.setLayout(null);
+        
+        
+        leftFunction.add(panelThem);
+        leftFunction.add(panelXoa);
+        leftFunction.add(panelSua);
+        leftFunction.add(panelXuatExcel);
+        
+        
+        //Thêm các nút chức năng vào các panel ở leftFunction
+        JButton buttonThem, buttonXoa, buttonSua, buttonXuatExcel;
+        buttonThem = new JButton("+");
+        buttonThem.setBounds(0,0,60,40);
+        buttonXoa = new JButton("-");
+        buttonXoa.setBounds(0,0,60,40);
+        buttonSua = new JButton("Sửa");
+        buttonSua.setBounds(0,0,60,40);
+        buttonXuatExcel = new JButton("Xuất Excel");
+        buttonXuatExcel.setBounds(0,0,70,40);
+        
+        panelThem.add(buttonThem);
+        panelXoa.add(buttonXoa);
+        panelSua.add(buttonSua);
+        panelXuatExcel.add(buttonXuatExcel);
+        
+        //Thêm các label mô tả các nút chức năng
+        JLabel labelThem, labelXoa, labelSua, labelXuatExcel;
+        labelThem = new JLabel("    Thêm");
+        labelThem.setBounds(0,40,80,20);
+        labelXoa = new JLabel("      Xóa");
+        labelXoa.setBounds(0,40,80,20);
+        labelSua = new JLabel("   Sửa");
+        labelSua.setBounds(8,40,80,20);
+        labelXuatExcel = new JLabel("   Xuất Excel");
+        labelXuatExcel.setBounds(-5,40,80,20);
+        
+        panelThem.add(labelThem);
+        panelXoa.add(labelXoa,BorderLayout.SOUTH);
+        panelSua.add(labelSua,BorderLayout.SOUTH);
+        panelXuatExcel.add(labelXuatExcel,BorderLayout.SOUTH);
+        
+        
+        
+        //Tạo và thêm các panel chứa các chức năng tìm kiếm
+        JPanel panelCombobox = new JPanel();
+        JPanel panelSearchbox = new JPanel();
+        JPanel panelSeachButton = new JPanel();
+        panelCombobox.setBackground(Color.LIGHT_GRAY);
+        panelSearchbox.setBackground(Color.MAGENTA);
+        panelSeachButton.setBackground(Color.WHITE);
+        
+        gbc = new GridBagConstraints(); // Resetting GridBagConstraints
+
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.weightx = 0.3;
         gbc.weighty = 0.3;
-        gbc.fill = GridBagConstraints.BOTH;
-        leftFunction.add(panelThem, gbc);
+        gbc.fill = GridBagConstraints.BOTH; 
+        rightFunction.add(panelCombobox, gbc);
+        gbc.weightx = 0.5;
+        gbc.weighty = 0.5;
         gbc.gridx = 1;
-        leftFunction.add(panelXoa, gbc);
+        rightFunction.add(panelSearchbox, gbc);
+        gbc.weightx = 0.2;
+        gbc.weighty = 0.2;
         gbc.gridx = 2;
-        leftFunction.add(panelSua, gbc);
-        gbc.gridx = 3;
-        leftFunction.add(panelXuatExcel, gbc);
+        rightFunction.add(panelSeachButton, gbc);
+        
+        
     }
-    
     
 }
